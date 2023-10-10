703e15c8a3d2672c07c9fbf718594ff6b0a9282c
jasondouglasgibbs <jasondouglasgibbs@gmail.com>
2023-10-10 11:17:27 -0400

    Added in additional API queries, including for weather alerts.

--- APIQuery.R
+++ APIQuery.R
@@ -26,3 +26,23 @@ HourlyForeCastcontent<-FROM_GeoJson(HourlyForceCastresponse)
 HourlyForeCastcontent<-HourlyForeCastcontent$properties$periods
 HourlyForeCastcontent<-lapply(HourlyForeCastcontent, function(x) {unlist(x)})
 HourlyForeCastcontent<-dplyr::bind_rows(HourlyForeCastcontent)
+
+
+##Forecast Grid Data.##
+##Retrieve the hourly forecast URL from the overaching API call.##
+ForecastGridDataURL<-content$properties$forecastGridData
+##Call the API and transform the resulting data into a DF.##
+ForceCastGridresponse<-as.character(RETRY("GET", ForecastGridDataURL, encode="json",time=10))
+ForceCastGridcontent<-FROM_GeoJson(HourlyForceCastresponse)
+
+
+##Alert Query##
+AlertURL<-paste0("https://api.weather.gov/alerts/active?area=",State)
+
+Alertresponse<-as.character(RETRY("GET", AlertURL, encode="json",time=10))
+AlertContent<-jsonlite::fromJSON(Alertresponse)
+AlertDF<-unlist(lapply(AlertContent$features$properties, function(x) {unlist(x)}))
+AlertDF<-dplyr::bind_rows(AlertDF)
+
+AlertStatement<-paste0(AlertContent$title,": ", AlertDF$description)
+AlertStatement
