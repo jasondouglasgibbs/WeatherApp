0b2617290dfdd13d33ce3ce7fb39ac787fbf94c0
Jason G <jasondouglasgibbs@gmail.com>
2024-07-27 10:17:44 -0400

    -Added AQI plot to initial .R file in preparation for implementation in the App.

--- airquality.R
+++ airquality.R
@@ -3,18 +3,25 @@
 AQItoken<-"6640265ae7ee49e61eed86a7ce4d2687ed07d3ba"
 
 
-##Builds base URL for forecast information.##
+##Builds base URL for AQI information and queries the API.##
 BaseURL<-paste0("http://api.waqi.info/feed/geo:", Lat$latitude,";", Long$longitude,"/?token=",AQItoken)
 response<-as.character(RETRY("GET", BaseURL, encode="json",time=10))
 content<-jsonlite::fromJSON(response)
 AQI<-content$data$aqi
 AQI
-
+##Creates dataframes for each pollutant that is forecasted and binds them together##
 AQIO3Fore<-content$data$forecast$daily$o3
-AQIO3Fore
+AQIO3Fore$pollutant<-"o3"
 AQIpm10Fore<-content$data$forecast$daily$pm10
-AQIpm10Fore
+AQIpm10Fore$pollutant<-"pm10"
 AQIpm25Fore<-content$data$forecast$daily$pm25
-AQIpm25Fore
+AQIpm25Fore$pollutant<-"pm2.5"
+AQIForecast<-bind_rows(AQIO3Fore,AQIpm10Fore,AQIpm25Fore)
+AQIForecast$day<-as.Date(AQIForecast$day)
 
-AQILocation<-content$data$city$name
+##Creates a plot of the AQI data.##
+AQIPlot<-ggplot(data=AQIForecast, aes(x=day, y = max, color=pollutant))+
+  geom_line()+xlab("Date")+ylab("Individual AQI by Pollutant")
+AQIPlotly<-ggplotly(AQIPlot)
+AQIPlotly
+
